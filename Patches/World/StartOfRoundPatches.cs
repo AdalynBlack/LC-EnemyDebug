1e084a663d3a38fb9a1e2736e3838de9390fe560
TonyGamer <tomblackaz@gmail.com>
2024-04-20 11:55:40 -0700

    Made debug drawings not dependent on ShowPathingNodes

--- Patches/World/StartOfRoundPatches.cs
+++ Patches/World/StartOfRoundPatches.cs
@@ -19,17 +19,17 @@ public class StartOfRoundPatches
 	[HarmonyPostfix]
 	static void UpdatePostfixPatch()
 	{
-		if(!EnemyDebugConfig.ShowPathingNodes.Value)
-			return;
-
-		foreach (var insideNode in RoundManager.Instance.insideAINodes)
+		if(EnemyDebugConfig.ShowPathingNodes.Value)
 		{
-			Draw.Sphere(insideNode.transform.position, 0.25f, color: new Color(0.5f, 0.5f, 0.5f));
-		}
+			foreach (var insideNode in RoundManager.Instance.insideAINodes)
+			{
+				Draw.Sphere(insideNode.transform.position, 0.25f, color: new Color(0.5f, 0.5f, 0.5f));
+			}
 
-		foreach (var outsideNode in RoundManager.Instance.outsideAINodes)
-		{
-			Draw.Sphere(outsideNode.transform.position, 0.25f, color: new Color(0.5f, 0.5f, 0.5f));
+			foreach (var outsideNode in RoundManager.Instance.outsideAINodes)
+			{
+				Draw.Sphere(outsideNode.transform.position, 0.25f, color: new Color(0.5f, 0.5f, 0.5f));
+			}
 		}
 		Draw.DrawAll();
 	}
